b3d2538bf0232da467709261a6169bd30d16c25c
elenafiodorova <fiodorova.elena.25.08@gmail.com>
2020-03-22 16:03:14 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( Array.isArray(array) === false || array === undefined || array.length === 0)  {return 0;}
+    else {
+        let minNumber=array[0];
+        for (let i=0; i<array.length; i++) {
+            if (minNumber>array[i+1]) {
+            minNumber = array[i+1];
+            } 
+        }
+        return minNumber;
+    }   
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( Array.isArray(array) === false || array === undefined || array.length === 0)  {return 0;}
+    else {
+        let maxNumber=array[0];
+        for (let i=0; i<array.length; i++) {
+            if (maxNumber<array[i+1]) {
+            maxNumber = array[i+1];
+            } 
+        }
+        return maxNumber;
+    }    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( Array.isArray(array) === false || array === undefined || array.length === 0)  {return 0;}
+    else {
+        let avgNumber = 0;
+        for (let i=0; i<array.length; i++) {
+            avgNumber = avgNumber + array[i];
+        }
+        avgNumber = avgNumber/array.length;
+        return avgNumber;
+    }    
 }
